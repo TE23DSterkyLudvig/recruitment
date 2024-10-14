a78a7666d6ebd12b8db5bb04d7ea235c8ba68e40
Ludvig Sterky <Ludvig.Sterky@elev.ga.ntig.se>
2024-10-14 09:53:11 +0200

    börjat

---
+++ recruitment/Program.cs
@@ -0,0 +1,40 @@
+﻿//List<string> names = ["Ludde", "Kotte", "Bertil"];
+
+// names[1] = "bertina";
+
+// names.Add("luddeman");
+// names.Add("Robust");
+// names.Remove("Ludde");
+
+// //System.Console.WriteLine(string.Join("\n",names));
+
+
+// for (int i = 0; i < names.Count; i++)
+// {
+//    Console.WriteLine(names[i]) ;
+// }
+
+
+//String name = Console.ReadLine();
+//if (name == names[1]){
+   // System.Console.WriteLine("Hej dummer!");
+//}
+
+//System.Console.WriteLine(names[1]);
+
+// Console.ReadLine();
+
+
+
+List<string>recruites = ["John", "Johan", "Josef", "Jonkler"];
+List<string>platser =[""];
+
+System.Console.WriteLine("Du är sergant för Helldivers och din uppgift är att överskåda nya recruits och sätta dem i tjänst.");
+Thread.Sleep(800);
+System.Console.WriteLine("Det är fyra stycken recruits men du har bara 3 platser.");
+Thread.Sleep(800);
+System.Console.WriteLine("Du kan välja mellan Nummer 1 John, nummer 2 Johan, nummer 3 Josef eller nummer 4 Jonkler.");
+
+string namn = Console.ReadLine();
+
+Console.ReadLine();
